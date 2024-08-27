3d25baeff48a25015b812f17937e6327a5e8df69
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-27 15:55:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// let arr=[1, 2, 3, 4];
+// let s=arr.filter((x)=>x%2===0);
+// let x=document.getElementById("output");
+function getNumbers() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  });
+}
