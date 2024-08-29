8e6df7adba9f4bb35c1beeef2cd402f10ea002fe
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 11:12:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-//your JS code here. If required.
-// let arr=[1, 2, 3, 4];
-// let s=arr.filter((x)=>x%2===0);
-// let x=document.getElementById("output");
-// function getNumbers() {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve([1, 2, 3, 4]);
-//     }, 3000);
-//   });
+function getNumbers() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve([1, 2, 3, 4]);
+        }, 3000);
+      });
+    }
 
-getNumbers()
-.then(numbers => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      // filter out odd numbers
-      const evenNumbers = numbers.filter(number => number % 2 === 0);
-      resolve(evenNumbers);
-    }, 1000);
-  });
-})
-.then(evenNumbers => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      // multiply even numbers by 2
-      const multipliedNumbers = evenNumbers.map(number => number * 2);
-      resolve(multipliedNumbers);
-    }, 2000);
-  });
-})
-.then(result => {
-  // update the HTML element with the result
-  document.getElementById('output').innerHTML = result.join(', ');
-});
-}
+    getNumbers().then(numbers => {
+      return numbers.filter(number => number % 2 === 0);
+    }).then(evenNumbers => {
+      return evenNumbers.map(number => number * 2);
+    }).then(finalNumbers => {
+      document.getElementById('output').innerText = finalNumbers.join(', ');
+    });
