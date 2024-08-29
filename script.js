195c9387a06518038f5ba933ef1907554d334a30
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 11:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-function getNumbers() {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([1, 2, 3, 4]);
-        }, 3000);
-      });
-    }
+// function getNumbers() {
+//       return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//           resolve([1, 2, 3, 4]);
+//         }, 3000);
+//       });
+//     }
+//     getNumbers().then(numbers => {
+//       return numbers.filter(number => number % 2 === 0);
+//     }).then(evenNumbers => {
+//       return evenNumbers.map(number => number * 2);
+//     }).then(finalNumbers => {
+//       document.getElementById('output').innerText = finalNumbers.join(', ');
+//     });
 
-    getNumbers().then(numbers => {
-      return numbers.filter(number => number % 2 === 0);
-    }).then(evenNumbers => {
-      return evenNumbers.map(number => number * 2);
-    }).then(finalNumbers => {
-      document.getElementById('output').innerText = finalNumbers.join(', ');
+function manipulateData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  })
+  .then(array => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(array.filter(num => num % 2 === 0));
+      }, 1000);
     });
+  })
+  .then(array => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(array.map(num => num * 2));
+      }, 2000);
+    });
+  });
+}
+
+manipulateData().then(result => {
+  document.getElementById('output').innerText = result.join(', ');
+});
